076ec063e6cc7314a549eaaddd503fccaffb52a7
Akshar Gabani <akshargabanistar945@gmail.com>
2023-09-01 09:24:01 +0530

    cart design

--- components/nav2.js
+++ components/nav2.js
@@ -2,7 +2,7 @@ const navbar = ()=>{
     return `
     <nav class="navbar navbar-expand-lg bg-primary">
             <div class="container d-flex justify-content-between">
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand" href="../index.html">
                     <img src="../img/sdLatestLogo.svg" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -18,7 +18,7 @@ const navbar = ()=>{
                         </form>
                     </div>
                     <div class="d-flex align-items-center">
-                        <div class="border-end px-3"><a href="" class="text-white text-capitalize fs-6 px-2"><i class="bi bi-cart2 fs-4 px-2"></i>cart</a></div>
+                        <div class="border-end px-3"><a href="../pages/cart.html" class="text-white text-capitalize fs-6 px-2"><i class="bi bi-cart2 fs-4 px-2"></i>cart</a></div>
                         <div class="px-3"><a href="../pages/signup.html" class="text-white text-capitalize fs-6"> <i class="bi bi-person-circle fs-4 px-2"> </i>sing-up</i></a></div>  
                     </div>
                 </div>
